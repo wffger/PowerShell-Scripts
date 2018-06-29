ee868791c6b9a98ddf5393b927365ce8bbc726d6
wffger <wenfei182@163.com>
2018-06-29 15:31:45 +0800

    转换编码
    将原文件转换为utf-8编码。

---
+++ PS5_convert2utf8.ps1
@@ -0,0 +1,28 @@
+﻿# (C) 2017 余东熹	https://github.com/wffger
+#
+# 此脚本转换当前目录文件为UTF-8编码
+# 
+############################### 
+$filePath = ".\*." + $args[0]
+$filePath
+$fileArray=@()
+$fileArray=Get-ItemPropertyValue -Path $filePath -Name name
+If($fileArray -is [array])
+{
+    For($i=0;$i -lt $fileArray.Count; $i++)
+    {
+        Write-Host "正在处理第$($i+1)个文件 : $($fileArray[$i])"
+        $filePath = ".\"+$fileArray[$i]
+        $fileContent = Get-Content -Path $filePath
+        $fileContent | Out-File -Encoding UTF8 -FilePath $filePath
+    }
+}
+Else
+{
+        Write-Host "正在处理文件 : $fileArray"
+        $filePath = ".\"+$fileArray
+        $fileContent = Get-Content -Path $filePath
+        $fileContent | Out-File -Encoding UTF8 -FilePath $filePath
+        #Get-Content -Path $filePath | Out-File -Encoding UTF8 -FilePath $filePath
+        Write-Host "$fileArray 处理完毕。"
+}
